1aba1ed873f66f41bef339eadb3ebac7755c1276
ashik77771 <74649564+ashik77771@users.noreply.github.com>
2021-10-07 12:31:04 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const toggle = document.getElementById('toggle');
+		  const navbar = document.getElementById('navbar');
+		
+		
+		
+		      toggle.onclick = function(){
+		          toggle.classList.toggle('active');
+		           navbar.classList.toggle('active');
+		      }
+		      const hamburger = document.getElementById('hamburger');
+		  const catagory = document.getElementById('catagory-section');
+		
+		
+		
+		      hamburger.onclick = function(){
+		          hamburger.classList.toggle('active');
+		           catagory.classList.toggle('active');
+		      }
